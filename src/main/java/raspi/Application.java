138b334c0e4627b9017d7d87df82bfafd2884eb1
outroXD <keisuke.nakasuga+githubXD@gmail.com>
2020-08-17 01:23:32 +0900

    少しリファクタリング。

--- src/main/java/raspi/Application.java
+++ src/main/java/raspi/Application.java
@@ -6,9 +6,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @ComponentScan
